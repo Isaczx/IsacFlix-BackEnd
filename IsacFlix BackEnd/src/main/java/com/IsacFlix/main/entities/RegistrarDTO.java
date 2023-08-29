9d18360f8bb2c83bdb07f1d52bc9d4433bfc9697
Isac Santos <118670709+Isaczx@users.noreply.github.com>
2023-08-28 21:11:14 -0300

    Delete IsacFlix BackEnd directory

--- IsacFlix BackEnd/src/main/java/com/IsacFlix/main/entities/RegistrarDTO.java
+++
@@ -1,5 +0,0 @@
-package com.IsacFlix.main.entities;
-
-public record RegistrarDTO(String name, String email , String CPF , String senha) {
-
-}
--- IsacFlix BackEnd/src/main/java/com/IsacFlix/main/entities/loginResponseDTO.java
+++
@@ -1,5 +0,0 @@
-package com.IsacFlix.main.entities;
-
-public record loginResponseDTO(String token) {
-
-}
